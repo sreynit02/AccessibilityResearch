d5014bc35da4f4939901046c5899082412b9b02a
Sreynit Khatt <khatts@berea.edu>
2022-07-19 18:04:03 -0500

    check angle from player to target and added sounds

--- Assets/FovManager.cs
+++ Assets/FovManager.cs
@@ -8,12 +8,10 @@ public class FovManager : MonoBehaviour
     public Transform[] target;
 
     public float viewAngle;
-    public float viewRange;
 
     void Update()
     {
       CanSeeTarget(target, viewAngle);
-
     }
 
 
@@ -27,6 +25,44 @@ public class FovManager : MonoBehaviour
             //Debug.DrawRay(transform.position, toTarget, Color.green);
             //Debug.Log(obj.name);
             obj.GetComponent<Outline>().enabled = true;
+            // float angle = Vector3.Angle(toTarget, transform.forward);
+            // if (angle<5.0f){
+            //     print("There's an obstacle in front");
+            //     //Debug.Log(obj.name);
+            // }
+            // else if(angle>5.0f)
+            // {
+            //   print("here");
+            //   Debug.Log(obj.name);
+            // }
+            // else if(angle>-5.0f){
+            //   print("angle????");
+            //   Debug.Log(obj.name);
+            // }
+            // else if(angle<-5.0f)
+            // {
+            //   print("hello!!!");
+            //   Debug.Log(obj.name);
+            // }
+
+            //project angle of target to XY plane
+            Vector3 projectedVector = Vector3.ProjectOnPlane(toTarget, transform.forward);
+            //calculate angle
+            float xyAngle = Vector3.SignedAngle(projectedVector, transform.up, transform.forward);
+            print(xyAngle);
+
+            if (xyAngle == 90)
+            {
+              print("There's an object in front of you!");
+            }
+            else if(xyAngle == 0 )
+            {
+              print("There's an object to your right!");
+            }
+            else if(xyAngle == 180)
+            {
+              print("There's an object to your left!");
+            }
           }else{
               obj.GetComponent<Outline>().enabled = false;
           }
